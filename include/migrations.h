a9a0f8c22e4cb15dc73ad54db5a53076f6b0ae39
Nathan Du <nathandu@outlook.com>
2022-12-13 11:11:40 +0800

    db: add migrations

---
+++ include/migrations.h
@@ -0,0 +1,46 @@
+/* SPDX-License-Identifier: MIT
+ * Copyright (C) 2022 McEndu et al.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice
+ * (including the next paragraph) shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef _AREF__MIGRATIONS_H
+#define _AREF__MIGRATIONS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <sqlite3.h>
+
+typedef struct aref_migration {
+	int (*up)(sqlite3 *cursor);
+	int (*down)(sqlite3 *cursor);
+} aref_migration;
+
+extern const aref_migration aref_dbmigration_initial_migration;
+
+extern const aref_migration *aref_migrations[];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !_AREF__MIGRATIONS_H */
---
+++ src/migrations/20221213_initial_migration.c
@@ -0,0 +1,61 @@
+/* SPDX-License-Identifier: MIT
+ * Copyright (C) 2022 McEndu et al.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice
+ * (including the next paragraph) shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <migrations.h>
+#include <stdlib.h>
+
+static int up(sqlite3 *cursor)
+{
+	return sqlite3_exec(
+		cursor,
+		u8"BEGIN TRANSACTION;"
+
+		"CREATE TABLE aref_metadata ("
+		"   key TEXT PRIMARY KEY,"
+		// rely on SQLite's dynamicness as we could
+		// store anything in our metadata table
+		"   value BLOB);"
+
+		"CREATE TABLE mappool ("
+		"   mapcode CHAR(6) PRIMARY KEY,"
+		"   mode INTEGER,"
+		"   beatmapid INTEGER);"
+
+		// set migration revision
+		"REPLACE INTO metadata (key, value) VALUES('revision', 0);"
+
+		"COMMIT TRANSACTION;",
+		NULL, NULL, NULL);
+}
+
+static int down(sqlite3 *cursor)
+{
+	return sqlite3_exec(cursor,
+						u8"BEGIN TRANSACTION;"
+						"DROP TABLE mappool;"
+						"DROP TABLE aref_metadata;"
+						"COMMIT TRANSACTION;",
+						NULL, NULL, NULL);
+}
+
+extern const aref_migration aref_dbmigration_initial_migration = {up, down};
